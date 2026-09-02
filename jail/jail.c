Juff-Ma/procd

commit ea7a790f210c6540d01e029cd6e93cea145ccf8b

jail: add support for running OCI bundle
Prepare ujail for running OCI bundled Linux containers.
This adds handling of most of the JSON schema defined by the
Open Container Initiative Runtime Specification.

What is supported by this commits:
 * basic OCI process definition
 * seccomp filters (no args yet)
 * capabilities (100%)
 * namespaces (100%)
 * uid/gid mappings for userns (100%)
 * mounts (no free form mounts yet)
 * env (100%, limited to a low number entries)
 * hostname (100%)
 * terminal (no consoleSize yet)

What is still missing:
 * complex mounts
 * maskedPaths, readonlyPaths
 * referencing existing namespaces
 * all hooks
 * rlimits
 * oomScoreAdj
 * additionalGids
 * cgroups
 * devices
 * sysctl
 * rootfsPropagation
 * personality and bi-arch (ie. 32-bit container on 64-bit host)

Signed-off-by: Daniel Golle <daniel@makrotopia.org>

jail/seccomp-oci.c
@@ -1,0 +1,272 @@
+/*
+ * parse and setup OCI seccomp filter
+ * Copyright (c) 2020 Daniel Golle <daniel@makrotopia.org>
+ * seccomp example with syscall reporting
+ * Copyright (c) 2012 The Chromium OS Authors <chromium-os-dev@chromium.org>
+ * Authors:
+ *  Kees Cook <keescook@chromium.org>
+ *  Will Drewry <wad@chromium.org>
+ *
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+#define _GNU_SOURCE 1
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <libubox/utils.h>
+#include <libubox/blobmsg.h>
+#include <libubox/blobmsg_json.h>
+
+#include "log.h"
+#include "seccomp-bpf.h"
+#include "seccomp-oci.h"
+#include "../syscall-names.h"
+#include "seccomp-syscalls-helpers.h"
+
+static uint32_t resolve_action(char *actname)
+{
+	if (!strcmp(actname, "SCMP_ACT_KILL"))
+		return SECCOMP_RET_KILL;
+	else if (!strcmp(actname, "SCMP_ACT_KILL_PROCESS"))
+		return SECCOMP_RET_KILLPROCESS;
+	else if (!strcmp(actname, "SCMP_ACT_TRAP"))
+		return SECCOMP_RET_TRAP;
+	else if (!strcmp(actname, "SCMP_ACT_ERRNO"))
+		return SECCOMP_RET_ERRNO;
+	else if (!strcmp(actname, "SCMP_ACT_ERROR"))
+		return SECCOMP_RET_ERRNO;
+	else if (!strcmp(actname, "SCMP_ACT_TRACE"))
+		return SECCOMP_RET_TRACE;
+	else if (!strcmp(actname, "SCMP_ACT_ALLOW"))
+		return SECCOMP_RET_ALLOW;
+	else if (!strcmp(actname, "SCMP_ACT_LOG"))
+		return SECCOMP_RET_LOGALLOW;
+	else {
+		ERROR("unknown seccomp action %s\n", actname);
+		return SECCOMP_RET_KILL;
+	}
+}
+
+static uint32_t resolve_architecture(char *archname)
+{
+	if (!strcmp(archname, "SCMP_ARCH_X86"))
+		return AUDIT_ARCH_I386;
+	else if (!strcmp(archname, "SCMP_ARCH_X86_64"))
+		return AUDIT_ARCH_X86_64;
+	else if (!strcmp(archname, "SCMP_ARCH_X32"))
+		/*
+		 * return AUDIT_ARCH_X86_64;
+		 * 32-bit userland on 64-bit kernel is not supported yet
+		 */
+		return 0;
+	else if (!strcmp(archname, "SCMP_ARCH_ARM"))
+		return AUDIT_ARCH_ARM;
+	else if (!strcmp(archname, "SCMP_ARCH_AARCH64"))
+		return AUDIT_ARCH_AARCH64;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPS"))
+		return AUDIT_ARCH_MIPS;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPS64"))
+		return AUDIT_ARCH_MIPS64;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPS64N32"))
+		return AUDIT_ARCH_MIPS64N32;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPSEL"))
+		return AUDIT_ARCH_MIPSEL;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPSEL64"))
+		return AUDIT_ARCH_MIPSEL64;
+	else if (!strcmp(archname, "SCMP_ARCH_MIPSEL64N32"))
+		return AUDIT_ARCH_MIPSEL64N32;
+	else if (!strcmp(archname, "SCMP_ARCH_PPC"))
+		return AUDIT_ARCH_PPC;
+	else if (!strcmp(archname, "SCMP_ARCH_PPC64"))
+		return AUDIT_ARCH_PPC64;
+	else if (!strcmp(archname, "SCMP_ARCH_PPC64LE"))
+		return AUDIT_ARCH_PPC64LE;
+	else if (!strcmp(archname, "SCMP_ARCH_S390"))
+		return AUDIT_ARCH_S390;
+	else if (!strcmp(archname, "SCMP_ARCH_S390X"))
+		return AUDIT_ARCH_S390X;
+	else if (!strcmp(archname, "SCMP_ARCH_PARISC"))
+		return AUDIT_ARCH_PARISC;
+	else if (!strcmp(archname, "SCMP_ARCH_PARISC64"))
+		return AUDIT_ARCH_PARISC64;
+	else {
+		ERROR("unknown seccomp architecture %s\n", archname);
+		return 0;
+	}
+}
+
+enum {
+	OCI_LINUX_SECCOMP_DEFAULTACTION,
+	OCI_LINUX_SECCOMP_ARCHITECTURES,
+	OCI_LINUX_SECCOMP_FLAGS,
+	OCI_LINUX_SECCOMP_SYSCALLS,
+	__OCI_LINUX_SECCOMP_MAX,
+};
+
+static const struct blobmsg_policy oci_linux_seccomp_policy[] = {
+	[OCI_LINUX_SECCOMP_DEFAULTACTION] = { "defaultAction", BLOBMSG_TYPE_STRING },
+	[OCI_LINUX_SECCOMP_ARCHITECTURES] = { "architectures", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_SECCOMP_FLAGS] = { "flags", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_SECCOMP_SYSCALLS] = { "syscalls", BLOBMSG_TYPE_ARRAY },
+};
+
+enum {
+	OCI_LINUX_SECCOMP_SYSCALLS_NAMES,
+	OCI_LINUX_SECCOMP_SYSCALLS_ACTION,
+	OCI_LINUX_SECCOMP_SYSCALLS_ERRNORET,
+	OCI_LINUX_SECCOMP_SYSCALLS_ARGS,
+	__OCI_LINUX_SECCOMP_SYSCALLS_MAX
+};
+
+static const struct blobmsg_policy oci_linux_seccomp_syscalls_policy[] = {
+	[OCI_LINUX_SECCOMP_SYSCALLS_NAMES] = { "names", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ERRNORET] = { "errnoRet", BLOBMSG_TYPE_INT32 },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ARGS] = { "args", BLOBMSG_TYPE_ARRAY },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ACTION] = { "action", BLOBMSG_TYPE_STRING },
+};
+
+enum {
+	OCI_LINUX_SECCOMP_SYSCALLS_ARGS_INDEX,
+	OCI_LINUX_SECCOMP_SYSCALLS_ARGS_VALUE,
+	OCI_LINUX_SECCOMP_SYSCALLS_ARGS_VALUETWO,
+	OCI_LINUX_SECCOMP_SYSCALLS_ARGS_OP,
+	__OCI_LINUX_SECCOMP_SYSCALLS_ARGS_MAX
+};
+
+static const struct blobmsg_policy oci_linux_seccomp_syscalls_args_policy[] = {
+	[OCI_LINUX_SECCOMP_SYSCALLS_ARGS_INDEX] = { "index", BLOBMSG_TYPE_INT32 },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ARGS_VALUE] = { "value", BLOBMSG_TYPE_INT64 },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ARGS_VALUETWO] = { "valueTwo", BLOBMSG_TYPE_INT64 },
+	[OCI_LINUX_SECCOMP_SYSCALLS_ARGS_OP] = { "op", BLOBMSG_TYPE_STRING },
+};
+
+struct sock_fprog *parseOCIlinuxseccomp(struct blob_attr *msg)
+{
+	struct blob_attr *tb[__OCI_LINUX_SECCOMP_MAX];
+	struct blob_attr *tbn[__OCI_LINUX_SECCOMP_SYSCALLS_MAX];
+	struct blob_attr *tba[__OCI_LINUX_SECCOMP_SYSCALLS_ARGS_MAX];
+	struct blob_attr *cur, *curn, *curarg;
+	int rem, remn, remargs, sc;
+	struct sock_filter *filter;
+	struct sock_fprog *prog;
+	int sz = 5, idx = 0;
+	uint32_t default_policy = 0;
+	uint32_t seccomp_arch;
+
+	blobmsg_parse(oci_linux_seccomp_policy, __OCI_LINUX_SECCOMP_MAX, tb, blobmsg_data(msg), blobmsg_len(msg));
+
+	if (!tb[OCI_LINUX_SECCOMP_DEFAULTACTION]) {
+		ERROR("seccomp: no default action set\n");
+		return NULL;
+	}
+
+	default_policy = resolve_action(blobmsg_get_string(tb[OCI_LINUX_SECCOMP_DEFAULTACTION]));
+
+	/* verify architecture while ignoring the x86_64 anomaly for now */
+	blobmsg_for_each_attr(cur, tb[OCI_LINUX_SECCOMP_ARCHITECTURES], rem) {
+		seccomp_arch = resolve_architecture(blobmsg_get_string(cur));
+		/* take the first useful arch for now */
+		if (seccomp_arch)
+			break;
+	}
+
+	if (ARCH_NR != seccomp_arch) {
+		ERROR("seccomp architecture doesn't match system\n");
+		return NULL;
+	}
+
+	blobmsg_for_each_attr(cur, tb[OCI_LINUX_SECCOMP_SYSCALLS], rem) {
+		blobmsg_parse(oci_linux_seccomp_syscalls_policy, __OCI_LINUX_SECCOMP_SYSCALLS_MAX, tbn, blobmsg_data(cur), blobmsg_len(cur));
+		blobmsg_for_each_attr(curn, tbn[OCI_LINUX_SECCOMP_SYSCALLS_NAMES], remn)
+			sz += 2;
+
+		if (tbn[OCI_LINUX_SECCOMP_SYSCALLS_ARGS])
+			blobmsg_for_each_attr(curarg, tbn[OCI_LINUX_SECCOMP_SYSCALLS_ARGS], remargs)
+				sz++;
+	}
+
+	prog = malloc(sizeof(struct sock_fprog));
+	if (!prog)
+		return NULL;
+
+	filter = calloc(sz, sizeof(struct sock_filter));
+	if (!filter) {
+		ERROR("failed to allocate memory for seccomp filter\n");
+		goto errout2;
+	}
+
+	/* validate arch */
+	set_filter(&filter[idx++], BPF_LD + BPF_W + BPF_ABS, 0, 0, arch_nr);
+	set_filter(&filter[idx++], BPF_JMP + BPF_JEQ + BPF_K, 1, 0, ARCH_NR);
+	set_filter(&filter[idx++], BPF_RET + BPF_K, 0, 0, SECCOMP_RET_KILL);
+
+	/* get syscall */
+	set_filter(&filter[idx++], BPF_LD + BPF_W + BPF_ABS, 0, 0, syscall_nr);
+
+	blobmsg_for_each_attr(cur, tb[OCI_LINUX_SECCOMP_SYSCALLS], rem) {
+		uint32_t action;
+		blobmsg_parse(oci_linux_seccomp_syscalls_policy, __OCI_LINUX_SECCOMP_SYSCALLS_MAX, tbn, blobmsg_data(cur), blobmsg_len(cur));
+		action = resolve_action(blobmsg_get_string(tbn[OCI_LINUX_SECCOMP_SYSCALLS_ACTION]));
+		if (tbn[OCI_LINUX_SECCOMP_SYSCALLS_ERRNORET]) {
+			if (action != SECCOMP_RET_ERRNO)
+				goto errout1;
+
+			action = SECCOMP_RET_ERROR(blobmsg_get_u32(tbn[OCI_LINUX_SECCOMP_SYSCALLS_ERRNORET]));
+		} else if (action == SECCOMP_RET_ERRNO)
+			action = SECCOMP_RET_ERROR(EPERM);
+
+		blobmsg_for_each_attr(curn, tbn[OCI_LINUX_SECCOMP_SYSCALLS_NAMES], remn) {
+			sc = find_syscall(blobmsg_get_string(curn));
+			if (sc == -1) {
+				ERROR("unknown syscall '%s'\n", blobmsg_get_string(curn));
+				goto errout1;
+			}
+
+			/* add rule to filter */
+			set_filter(&filter[idx++], BPF_JMP + BPF_JEQ + BPF_K, 0, 1, sc);
+			set_filter(&filter[idx++], BPF_RET + BPF_K, 0, 0, action);
+
+		}
+		blobmsg_for_each_attr(curn, tbn[OCI_LINUX_SECCOMP_SYSCALLS_ARGS], remn) {
+			blobmsg_parse(oci_linux_seccomp_syscalls_args_policy, __OCI_LINUX_SECCOMP_SYSCALLS_ARGS_MAX, tba, blobmsg_data(curn), blobmsg_len(curn));
+			/* ToDo: process args */
+		}
+	}
+
+	set_filter(&filter[idx], BPF_RET + BPF_K, 0, 0, default_policy);
+
+	prog->len = (unsigned short) idx + 1;
+	prog->filter = filter;
+
+	return prog;
+
+errout1:
+	free(prog->filter);
+errout2:
+	free(prog);
+	return NULL;
+}
+
+
+int applyOCIlinuxseccomp(struct sock_fprog *prog)
+{
+	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
+		ERROR("prctl(PR_SET_NO_NEW_PRIVS) failed: %m\n");
+		goto errout;
+	}
+
+	if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, prog)) {
+		ERROR("prctl(PR_SET_SECCOMP) failed: %m\n");
+		goto errout;
+	}
+	free(prog);
+
+	return 0;
+
+errout:
+	free(prog->filter);
+	free(prog);
+	return errno;
+}

CMakeLists.txt
@@ -103,8 +103,12 @@ INSTALL(TARGETS preload-seccomp
 ADD_DEPENDENCIES(preload-seccomp syscall-names-h)
 endif()
 
+IF(SECCOMP_SUPPORT)
+  SET(SOURCES_OCI_SECCOMP jail/seccomp-oci.c)
+ENDIF()
+
 IF(JAIL_SUPPORT)
-ADD_EXECUTABLE(ujail jail/jail.c jail/elf.c jail/fs.c jail/capabilities.c)
+ADD_EXECUTABLE(ujail jail/jail.c jail/elf.c jail/fs.c jail/capabilities.c ${SOURCES_OCI_SECCOMP})
 TARGET_LINK_LIBRARIES(ujail ${ubox} ${ubus} ${blobmsg_json})
 INSTALL(TARGETS ujail
 	RUNTIME DESTINATION ${CMAKE_INSTALL_SBINDIR}

jail/capabilities.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2015 Etienne CHAMPETIER <champetier.etienne@gmail.com>
+ * Copyright (C) 2020 Daniel Golle <daniel@makrotopia.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 2.1
@@ -14,6 +15,7 @@
 #define _GNU_SOURCE 1
 #include <syslog.h>
 #include <sys/prctl.h>
+#include <sys/capability.h>
 
 #include <libubox/blobmsg.h>
 #include <libubox/blobmsg_json.h>
@@ -22,15 +24,185 @@
 #include "../capabilities-names.h"
 #include "capabilities.h"
 
+#define JAIL_CAP_ERROR (1LLU << (CAP_LAST_CAP+1))
+#define JAIL_CAP_ALL (0xffffffffffffffffLLU)
+
 static int find_capabilities(const char *name)
 {
 	int i;
 
 	for (i = 0; i <= CAP_LAST_CAP; i++)
-		if (capabilities_names[i] && !strcmp(capabilities_names[i], name))
+		if (capabilities_names[i] && !strcasecmp(capabilities_names[i], name))
 			return i;
 
 	return -1;
+}
+
+enum {
+	OCI_CAPABILITIES_BOUNDING,
+	OCI_CAPABILITIES_EFFECTIVE,
+	OCI_CAPABILITIES_INHERITABLE,
+	OCI_CAPABILITIES_PERMITTED,
+	OCI_CAPABILITIES_AMBIENT,
+	__OCI_CAPABILITIES_MAX
+};
+
+static const struct blobmsg_policy oci_capabilities_policy[] = {
+	[OCI_CAPABILITIES_BOUNDING] = { "bounding", BLOBMSG_TYPE_ARRAY },
+	[OCI_CAPABILITIES_EFFECTIVE] = { "effective", BLOBMSG_TYPE_ARRAY },
+	[OCI_CAPABILITIES_INHERITABLE] = { "inheritable", BLOBMSG_TYPE_ARRAY },
+	[OCI_CAPABILITIES_PERMITTED] = { "permitted", BLOBMSG_TYPE_ARRAY },
+	[OCI_CAPABILITIES_AMBIENT] = { "ambient", BLOBMSG_TYPE_ARRAY },
+};
+
+static uint64_t parseOCIcap(struct blob_attr *msg)
+{
+	struct blob_attr *cur;
+	int rem;
+	uint64_t caps = 0;
+	int capnum;
+
+	/* each capset is optional, set all-1 mask if absent */
+	if (!msg)
+		return JAIL_CAP_ALL;
+
+	blobmsg_for_each_attr(cur, msg, rem) {
+		capnum = find_capabilities(blobmsg_get_string(cur));
+		if (capnum < 0)
+			return JAIL_CAP_ERROR;
+
+		caps |= (1LLU << capnum);
+	}
+
+	return caps;
+}
+
+int parseOCIcapabilities(struct jail_capset *capset, struct blob_attr *msg)
+{
+	struct blob_attr *tb[__OCI_CAPABILITIES_MAX];
+	uint64_t caps;
+	blobmsg_parse(oci_capabilities_policy, __OCI_CAPABILITIES_MAX, tb, blobmsg_data(msg), blobmsg_len(msg));
+
+	caps = parseOCIcap(tb[OCI_CAPABILITIES_BOUNDING]);
+	if (caps == JAIL_CAP_ERROR)
+		return EINVAL;
+	else
+		capset->bounding = caps;
+
+	caps = parseOCIcap(tb[OCI_CAPABILITIES_EFFECTIVE]);
+	if (caps == JAIL_CAP_ERROR)
+		return EINVAL;
+	else
+		capset->effective = caps;
+
+	caps = parseOCIcap(tb[OCI_CAPABILITIES_INHERITABLE]);
+	if (caps == JAIL_CAP_ERROR)
+		return EINVAL;
+	else
+		capset->inheritable = caps;
+
+	caps = parseOCIcap(tb[OCI_CAPABILITIES_PERMITTED]);
+	if (caps == JAIL_CAP_ERROR)
+		return EINVAL;
+	else
+		capset->permitted = caps;
+
+	caps = parseOCIcap(tb[OCI_CAPABILITIES_AMBIENT]);
+	if (caps == JAIL_CAP_ERROR)
+		return EINVAL;
+	else
+		capset->ambient = caps;
+
+	capset->apply = 1;
+
+	return 0;
+}
+
+
+int applyOCIcapabilities(struct jail_capset ocicapset)
+{
+	struct __user_cap_header_struct uh = {};
+	struct __user_cap_data_struct ud;
+	int cap;
+	int is_set;
+
+	if (!ocicapset.apply)
+		return 0;
+
+	/* drop from bounding set */
+	if (ocicapset.bounding != JAIL_CAP_ALL) {
+		for (cap = 0; cap <= CAP_LAST_CAP; cap++) {
+			if (!prctl(PR_CAPBSET_READ, cap, 0, 0, 0)) {
+				/* can't raise */
+				if (ocicapset.bounding & (1LLU << cap))
+					ERROR("capability %s (%d) is not in bounding set\n", capabilities_names[cap], cap);
+
+				continue;
+			}
+			if ( (ocicapset.bounding & (1LLU << cap)) == 0) {
+				DEBUG("dropping capability %s (%d) from bounding set\n", capabilities_names[cap], cap);
+				if (prctl(PR_CAPBSET_DROP, cap, 0, 0, 0)) {
+					ERROR("prctl(PR_CAPBSET_DROP, %d) failed: %m\n", cap);
+					return errno;
+				}
+			} else {
+				DEBUG("keeping capability %s (%d)\n", capabilities_names[cap], cap);
+			}
+		}
+	}
+
+	/* set effective, permitted and inheritable */
+	uh.version = _LINUX_CAPABILITY_VERSION_3;
+	uh.pid = getpid();
+
+	if (capget(&uh, &ud)) {
+		ERROR("capget() failed\n");
+		return -1;
+	}
+
+	DEBUG("old capabilities: Pe=%08x Pp=%08x Pi=%08x\n", ud.effective, ud.permitted, ud.inheritable);
+
+	if (ocicapset.effective != JAIL_CAP_ALL)
+		ud.effective = ocicapset.effective;
+
+	if (ocicapset.permitted != JAIL_CAP_ALL)
+		ud.permitted = ocicapset.permitted;
+
+	if (ocicapset.inheritable != JAIL_CAP_ALL)
+		ud.inheritable = ocicapset.inheritable;
+
+	DEBUG("new capabilities: Pe=%08x Pp=%08x Pi=%08x\n", ud.effective, ud.permitted, ud.inheritable);
+
+	if (capset(&uh, &ud)) {
+		ERROR("capset() failed\n");
+		return -1;
+	}
+
+	/* edit ambient set */
+	if (ocicapset.ambient != JAIL_CAP_ALL) {
+		for (cap = 0; cap <= CAP_LAST_CAP; cap++) {
+			is_set = prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_IS_SET, cap, 0, 0);
+			if ( (ocicapset.ambient & (1LLU << cap)) == 0) {
+				if (is_set) {
+					DEBUG("dropping capability %s (%d) from ambient set\n", capabilities_names[cap], cap);
+					if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_LOWER, cap, 0, 0)) {
+						ERROR("prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_LOWER, %d, 0, 0) failed: %m\n", cap);
+						return errno;
+					}
+				}
+			} else {
+				if (!is_set) {
+					DEBUG("raising capability %s (%d) to ambient set\n", capabilities_names[cap], cap);
+					if (prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_RAISE, cap, 0, 0)) {\
+						ERROR("prctl(PR_CAP_AMBIENT, PR_CAP_AMBIENT_RAISE, %d, 0, 0) failed: %m\n", cap);
+						return errno;
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
 }
 
 int drop_capabilities(const char *file)
@@ -81,7 +253,7 @@ int drop_capabilities(const char *file)
 
 	if (capdrop == 0LLU) {
 		DEBUG("cap.keep empty -> only dropping capabilities from cap.drop (blacklist)\n");
-		capdrop = 0xffffffffffffffffLLU;
+		capdrop = JAIL_CAP_ALL;
 	} else {
 		DEBUG("cap.keep has at least one capability -> dropping every capabilities not in cap.keep (whitelist)\n");
 	}

jail/capabilities.h
@@ -13,6 +13,20 @@
 #ifndef _JAIL_CAPABILITIES_H_
 #define _JAIL_CAPABILITIES_H_
 
+#include <libubox/blobmsg.h>
+
+struct jail_capset {
+	uint64_t bounding;
+	uint64_t effective;
+	uint64_t inheritable;
+	uint64_t permitted;
+	uint64_t ambient;
+	uint8_t apply;
+};
+
 int drop_capabilities(const char *file);
+
+int parseOCIcapabilities(struct jail_capset *capset, struct blob_attr *msg);
+int applyOCIcapabilities(struct jail_capset capset);
 
 #endif

jail/seccomp-bpf.h
@@ -41,8 +41,10 @@
 #define SECCOMP_RET_TRAP	0x00030000U /* disallow and force a SIGSYS */
 #define SECCOMP_RET_ERRNO	0x00050000U /* returns an errno */
 #define SECCOMP_RET_LOG		0x00070000U
+#define SECCOMP_RET_LOGALLOW	0x7ffc0000U
 #define SECCOMP_RET_TRACE	0x7ff00000U /* pass to a tracer or disallow */
 #define SECCOMP_RET_ALLOW	0x7fff0000U /* allow */
+#define SECCOMP_RET_KILLPROCESS	0x80000000U
 #define SECCOMP_RET_ERROR(x)	(SECCOMP_RET_ERRNO | ((x) & 0x0000ffffU))
 #define SECCOMP_RET_LOGGER(x)	(SECCOMP_RET_LOG | ((x) & 0x0000ffffU))
 

jail/seccomp-oci.h
@@ -1,0 +1,33 @@
+/*
+ * Copyright (C) 2020 Daniel Golle <daniel@makrotopia.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _JAIL_SECCOMP_OCI_H_
+#define _JAIL_SECCOMP_OCI_H_
+
+#include <linux/filter.h>
+
+#ifdef SECCOMP_SUPPORT
+struct sock_fprog *parseOCIlinuxseccomp(struct blob_attr *msg);
+int applyOCIlinuxseccomp(struct sock_fprog *prog);
+#else
+
+
+struct sock_fprog *parseOCIlinuxseccomp(struct blob_attr *msg) {
+	return NULL;
+}
+
+int applyOCIlinuxseccomp(struct sock_fprog *prog) {
+	return ENOTSUP;
+}
+#endif
+
+#endif

jail/seccomp-syscalls-helpers.h
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2015 John Crispin <blogic@openwrt.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+#ifndef _JAIL_SECCOMP_HELPERS_H_
+#define _JAIL_SECCOMP_HELPERS_H_
+
+static int find_syscall(const char *name)
+{
+	int i;
+
+	for (i = 0; i < SYSCALL_COUNT; i++) {
+		int sc = syscall_index_to_number(i);
+		if (syscall_name(sc) && !strcmp(syscall_name(sc), name))
+			return sc;
+	}
+
+	return -1;
+}
+
+static void set_filter(struct sock_filter *filter, __u16 code, __u8 jt, __u8 jf, __u32 k)
+{
+	filter->code = code;
+	filter->jt = jt;
+	filter->jf = jf;
+	filter->k = k;
+}
+
+#endif

commit 4a6b6ea12d2bd21c25408a2df4d4d254ea4e95d7

jail: apply OCI seccomp without forcing no-new-privs
applyOCIlinuxseccomp() set PR_SET_NO_NEW_PRIVS unconditionally before
installing the filter, so a bundle with process.noNewPrivileges left
false still ended up with NoNewPrivs set once it carried a seccomp
profile. The runtime-tools default configuration is exactly that, and
runtimetest reads the flag back from /proc/self/status.

The kernel accepts SECCOMP_SET_MODE_FILTER either under no-new-privs
or from a task holding CAP_SYS_ADMIN in its user namespace, so the
flag is not needed to install a filter, only the ordering matters.
Follow runc: when noNewPrivileges is true, set the flag after the
capability drop and install the filter last, so the filter need not
permit the remaining setup syscalls; when it is false, install the
filter in the container init before the capability drop, while the
init still holds CAP_SYS_ADMIN in its own user namespace. The filter
then has to permit the syscalls used up to execve (capset, prctl,
setresuid, setgroups, umask, chdir, the landlock calls, execve), which
is the same requirement runc imposes with that ordering.

The ptrace injection path cannot serve the noNewPrivileges=false case
at all: it acts after execve, when the bounding set of a typical bundle
no longer contains CAP_SYS_ADMIN, which is why seccomp-inject sets the
flag itself. Enforcing bundle profiles without no-new-privs therefore
take the in-process route already used for SCMP_ACT_NOTIFY and
flag-carrying profiles, where the linker-extended profile stays in
place for the lifetime of the process instead of being narrowed at the
entry point. The early install is confined to OCI bundles: a plain
jail given a profile with -S also lands in opts.ociseccomp, and keeps
the injection path whether or not procd passed -c, exactly as before.

The CLOSE_RANGE_CLOEXEC sweep moves in front of the early install so
that marking the inherited descriptors close-on-exec does not depend
on the profile permitting close_range; the flag only takes effect at
execve, and nothing opened between the two points is meant to survive
it.

Containers with noNewPrivileges=true are unaffected: they keep the
flag, drop capabilities and get the filter last, whether injected or
in-process.

Fixes: ea7a790f210c ("jail: add support for running OCI bundle")

jail/jail.c
@@ -1446,6 +1446,7 @@ static void notify_signal(int fd)
 }
 
 static bool jail_ptrace_seccomp(void);
+static bool jail_inproc_seccomp(void);
 
 static void free_and_exit(int ret)
 {
@@ -2938,11 +2939,18 @@ static void post_jail_fs(void)
 static void post_start_hook(void)
 {
 	int pw_uid, pw_gid, gr_gid;
+	struct sock_fprog *seccomp_prog = opts.ociseccomp_linker ?: opts.ociseccomp;
 
 	if (opts.scheduler.set && applyOCIprocessscheduler())
 		free_and_exit(EXIT_FAILURE);
 
 	if (opts.ioprio.set && applyOCIprocessiopriority())
+		free_and_exit(EXIT_FAILURE);
+
+	syscall(SYS_close_range, 3, ~0U, CLOSE_RANGE_CLOEXEC);
+
+	if (seccomp_prog && jail_inproc_seccomp() && !opts.no_new_privs &&
+	    applyOCIlinuxseccomp(seccomp_prog, opts.name, opts.ocibundle))
 		free_and_exit(EXIT_FAILURE);
 
 	/*
@@ -3076,13 +3084,12 @@ static void post_start_hook(void)
 		free_and_exit(EXIT_FAILURE);
 	}
 
-	if (opts.ociseccomp && seccomp_oci_needs_inproc() &&
-	    applyOCIlinuxseccomp(opts.ociseccomp_linker ?: opts.ociseccomp, opts.name, opts.ocibundle))
+	if (seccomp_prog && jail_inproc_seccomp() && opts.no_new_privs &&
+	    applyOCIlinuxseccomp(seccomp_prog, opts.name, opts.ocibundle))
 		free_and_exit(EXIT_FAILURE);
 
 	uloop_end();
 	free_opts(false);
-	syscall(SYS_close_range, 3, ~0U, CLOSE_RANGE_CLOEXEC);
 	if (jail_ptrace_seccomp() && ptrace(PTRACE_TRACEME, 0, 0, 0)) {
 		ERROR("PTRACE_TRACEME failed: %m\n");
 		exit(EXIT_FAILURE);
@@ -7031,12 +7038,20 @@ static void post_create_runtime(void)
 		pipe_send_start_container(NULL);
 }
 
+static bool jail_inproc_seccomp(void)
+{
+	if (seccomp_oci_needs_inproc())
+		return true;
+
+	return opts.ocibundle && !opts.no_new_privs && opts.seccomp_mode == SECCOMP_MODE_ENFORCE;
+}
+
 static bool jail_ptrace_seccomp(void)
 {
 	if (opts.seccomp_mode == SECCOMP_MODE_TRACE)
 		return true;
 
-	return opts.ociseccomp && !seccomp_oci_needs_inproc();
+	return opts.ociseccomp && !jail_inproc_seccomp();
 }
 
 static void jail_seccomp_run(void)

jail/seccomp-oci.c
@@ -935,11 +935,6 @@ int applyOCIlinuxseccomp(struct sock_fprog *prog, const char *container_id,
 {
 	int listener_fd = -1;
 
-	if (prctl(PR_SET_NO_NEW_PRIVS, 1, 0, 0, 0)) {
-		ERROR("prctl(PR_SET_NO_NEW_PRIVS) failed: %m\n");
-		goto errout;
-	}
-
 	if (seccomp_uses_notify) {
 		if (!seccomp_listener_path) {
 			ERROR("seccomp: SCMP_ACT_NOTIFY used without listenerPath\n");